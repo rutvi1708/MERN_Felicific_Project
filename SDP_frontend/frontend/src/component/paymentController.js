f8651aef561a5c5e37cdefd8401b48da873f9939
Rohit2010 <59710890+Rohit2010@users.noreply.github.com>
2020-11-03 11:45:26 +0530

    Update paymentController.js

--- SDP_frontend/frontend/src/component/paymentController.js
+++ SDP_frontend/frontend/src/component/paymentController.js
@@ -1,106 +1,58 @@
-import React, { Component, Fragment } from "react";
+import React,  { Component } from 'react';
+import { Button } from 'reactstrap';
 import { connect } from "react-redux";
-//import { API } from "../../../../SDP_backend/routes/api";
-import { Redirect } from "react-router";
 
-class CheckOut extends React.Component {
+class Checkout extends Component {
+
+  // state = {
+  //   amount: 1
+  // };
+
   constructor() {
-    super();
+    super()
     this.openCheckout = this.openCheckout.bind(this);
   }
-  state = {
-    userId: "",
-    token: "",
-  };
-  componentDidMount() {
-    const data = JSON.parse(localStorage.getItem("token"));
-    console.log(data);
-    this.setState({ userId: data.user._id });
-    this.setState({ token: data.token });
-  }
 
-  openCheckout(history,userId,emptyCart, token,onlinePaymentOrderBody) {
+  
+
+  openCheckout() {
     let options = {
-      key: "rzp_test_ldJZM3zfK5SRfV",
-      amount: this.props.total * 100, // 2000 paise = INR 20, amount in paisa
-      name: "Pavan Joshi",
-      description: "Purchase Description",
-      image: "../img/ddulogo.png",
-      handler: function (response) {
-        console.log(JSON.stringify({
-          ...onlinePaymentOrderBody.Order,
-          transaction_id: response.razorpay_payment_id,
-        }));
-        if (response) {
-          fetch( {
-            method: "POST",
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-            },
-            body: JSON.stringify({
-              Order:{...onlinePaymentOrderBody.Order,
-              transaction_id: response.razorpay_payment_id,
-            payment:"PAID"}
-            }),
-          })
-            .then((response) => {
-              console.log(response);
-              alert("order placed Successfully")
-              emptyCart()
-              return (
-                <Redirect to="/home"/>
-              )
-              
-            })
-            .catch((err) => console.log(err));
-        }
-              alert("order placed Successfully")
-              emptyCart()
-              return (
-                <Redirect to="/home"/>
-              )
+      "key": "rzp_live_9l5GYyl4yGK5Pq",
+      "amount": 1*100, // 2000 paise = INR 20, amount in paisa
+      "name": "Pranav",
+      "description": "Purchase Description",
+      "image": "/your_logo.png",
+      "handler": function (response){
+        alert(response.razorpay_payment_id);
       },
-      prefill: {
-        name: "Pavan Joshi",
-        email: "pbjoshi222@gmail.com",
-        Number: "9427855633",
-        address: "",
+      "prefill": {
+        "name": "Pravnav Joshi",
+        "email": "pbjoshi222@gmail.com"
       },
-      notes: {
-        address: "",
+      "notes": {
+        "address": "Hello World"
       },
-      theme: {
-        color: "#F37254",
-      },
+      "theme": {
+        "color": "#F37254"
+      }
     };
-
+    
     let rzp = new window.Razorpay(options);
     rzp.open();
   }
-
-  render() {
-    const {userId, token} =this.state
-    const {onlinePaymentOrderBody, emptyCart,history} = this.props
+  
+  render () {
     return (
       <div>
-        <button class="block amber accent-3 center" onClick={()=>this.openCheckout(history,userId,emptyCart,token,onlinePaymentOrderBody)}>
-          Pay Rs. {this.props.total}
-        </button>
+        <Button onClick={this.openCheckout}>Pay Rs. {this.props.Amount}</Button> 
       </div>
-    );
+    )
   }
 }
 const mapStateToProps = (state) => {
+  console.log(state)
   return {
-    onlinePaymentOrderBody: state.auth.onlinePaymentOrderBody,
+    Amount:state.Amount
   };
 };
-const mapDispatchToProps = dispatch => {
-  return{
-    emptyCart: () => dispatch({type:'CART_EMPTY'}),
- 
-  }
-}
-export default connect(mapStateToProps,mapDispatchToProps)(CheckOut);
+export default connect(mapStateToProps, null)(Checkout);
